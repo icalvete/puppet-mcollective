4f0477dab7d8f42f12ae508687444a7508e41198
Israel Calvete <icalvete@gmail.com>
2013-11-10 10:55:50 +0100

    Fix #2

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,11 @@ class mcollective::install {
 
   realize Package['stomp']
 
+  package {'mcollective-plugins-facts-facter':
+    ensure => present,
+    before => Package[$mcollective::params::mcollective_package]
+  }
+
   package {$mcollective::params::mcollective_package:
     ensure  => present,
     require => Package['stomp']

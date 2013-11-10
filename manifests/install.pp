f69dc25b0e0750dc984f5cfb9f21059c6d62ccd4
Israel Calvete <icalvete@gmail.com>
2013-11-10 11:14:49 +0100

    Fix #2
    Diferent package name in each $::osfamily

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,9 +2,21 @@ class mcollective::install {
 
   realize Package['stomp']
 
-  package {'mcollective-plugins-facts-facter':
-    ensure => present,
-    before => Package[$mcollective::params::mcollective_package]
+  case $::osfamily {
+
+    /^(Debian|Ubuntu)$/: {
+      package {'mcollective-plugins-facts-facter':
+        ensure => present,
+        before => Package[$mcollective::params::mcollective_package]
+      }
+    }
+    /^(CentOS|RedHat)$/: {
+      package {'mcollective-facter-facts':
+        ensure => present,
+        before => Package[$mcollective::params::mcollective_package]
+      }
+    }
+    default: {}
   }
 
   package {$mcollective::params::mcollective_package:
